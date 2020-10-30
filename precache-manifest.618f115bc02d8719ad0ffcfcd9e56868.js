0f7a217acd8418e42d86e9a5afc904b2f148a4d2
Yeonju Song <yeonju@Yeonjuui-MacBookPro.local>
2020-10-30 15:48:25 +0900

    Updates

---
+++ precache-manifest.618f115bc02d8719ad0ffcfcd9e56868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9739e8b259f4d1a23152430ff3f347d",
+    "url": "/movieapp2020/index.html"
+  },
+  {
+    "revision": "5132306b07ce9be14571",
+    "url": "/movieapp2020/static/css/main.6ec08e7c.chunk.css"
+  },
+  {
+    "revision": "d8040bd826d81c8b747b",
+    "url": "/movieapp2020/static/js/2.a1ceab77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieapp2020/static/js/2.a1ceab77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5132306b07ce9be14571",
+    "url": "/movieapp2020/static/js/main.ea6a31b9.chunk.js"
+  },
+  {
+    "revision": "92d0a99e64891c82c30d",
+    "url": "/movieapp2020/static/js/runtime-main.ae3b6c42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.666b983d143ca699ef9ac45429cad33f.js"
+  "/movieapp2020/precache-manifest.618f115bc02d8719ad0ffcfcd9e56868.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movieapp2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
